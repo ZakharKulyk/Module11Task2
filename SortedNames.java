6ec5472d49040597fc0e47112e2a77e5abab6766
zkhr <zakhar864@gmail.com>
2024-05-01 11:47:58 +0200

    Method was added

--- SortedNames.java
+++ SortedNames.java
@@ -10,12 +10,14 @@ public class SortedNames {
         names.add("Victor");
         names.add("Zakhar");
 
+        System.out.println(UpperCaseAndReverseOrder(names));
+    }
+    static public List<String> UpperCaseAndReverseOrder(List<String> names){
         List<String> collect = names.stream()
                 .map(name -> name.toUpperCase())
                 .sorted(Comparator.reverseOrder())
                 .collect(Collectors.toList());
 
-        System.out.println(collect);
-
+        return  collect;
     }
 }
